b1c23dfb6c3a3c06522cd0825f1753a1da0e5475
Yang Shun <tay.yang.shun@gmail.com>
2013-12-21 02:03:12 +0800

    Fix bug of main page not showing up in firefox

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,6 @@ $(function() {
     for (var i = 1; i <= 5; i++) {
         var $div = $('<div></div>');
         $div.attr('class', 'ps-' + i.toString());
-        appendContent($div);
         function appendContent($div) {
             $('.main-container').append($div);
             $.get(makeFilePath(i), function(data) {
@@ -12,5 +11,6 @@ $(function() {
                 $div.append(html);
             });
         }
+        appendContent($div);
     }
 });
