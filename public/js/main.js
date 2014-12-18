0b203ce8cbcac11b0d421a8ecf8d467369d33ab6
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-12-19 02:16:56 +0800

    [Misc] Fix bug for highlighting on main page

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,24 @@
 var converter = new Showdown.converter();
 
 $(function() {
-    for (var i = 1; i <= 5; i++) {
+    var PS_COUNT = 5;
+    var count = 0;
+    function appendContent($div) {
+        $('.main-container').append($div);
+        $.get(makeFilePath(i), function(data) {
+            var html = converter.makeHtml(data);
+            $div.append(html);
+            count++;
+            if (count === PS_COUNT) {
+                $('pre code').each(function (i, block) {
+                    hljs.highlightBlock(block);
+                });
+            }
+        });
+    }
+    for (var i = 1; i <= PS_COUNT; i++) {
         var $div = $('<div></div>');
         $div.attr('class', 'ps-' + i.toString());
-        function appendContent($div) {
-            $('.main-container').append($div);
-            $.get(makeFilePath(i), function(data) {
-                var html = converter.makeHtml(data);
-                $div.append(html);
-            });
-        }
         appendContent($div);
-        $('pre code').each(function (i, block) {
-            hljs.highlightBlock(block);
-        });
     }
 });
