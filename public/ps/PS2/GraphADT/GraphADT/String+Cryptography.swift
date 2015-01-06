e2af6fc6b53d82dfd624c7527e40077dfc9775d4
Minh Tu Le <minhtule05@gmail.com>
2015-01-06 20:30:23 +0700

    [PS2] Removed private functions in String extension

--- public/ps/PS2/GraphADT/GraphADT/String+Cryptography.swift
+++ public/ps/PS2/GraphADT/GraphADT/String+Cryptography.swift
@@ -18,28 +18,4 @@ extension String {
         
         return "" // Please remove this line in actual implementation.
     }
-    
-    //  Return a tree with String labels such that when performing
-    //  a breath-first traversal on this tree, its labels appear in
-    //  the same order that they appear from left to right of the
-    //  string `self`.
-    //
-    //  In other words,
-    //  str.breadthFirstToTreeWithNumberOfChildren(n).breadthFirstToString() == str
-    private func breadthFirstToTreeWithNumberOfChildren(numberOfChildren: Int) -> Tree<String> {
-        
-        return Tree() // Please remove this line in actual implementation.
-    }
-    
-    //  Return a tree with String labels such that when performing
-    //  a depth-first traversal on this tree, its labels appear in
-    //  the same order that they appear from left to right of the
-    //  string `self`.
-    //
-    //  In other words,
-    //  str.depthFirstToTreeWithNumberOfChildren(n).depthFirstToString() == str
-    private func depthFirstToTreeWithNumberOfChildren(numberOfChildren: Int) -> Tree<String> {
-        
-        return Tree() // Please remove this line in actual implementation.
-    }
 }
